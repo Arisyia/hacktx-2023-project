e478a3e2961d430bbfcaba50effc48d678fbe796
unknown <faithnguyen814@gmail.com>
2023-10-21 15:57:03 -0500

    almost done with design!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import './App.css';
 
 function App() {
+  const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+  const {isLoaded} = useLoadScript({googleMapsApiKey: API_KEY});
+
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map />
     </div>
   );
 }
+
+function Map() {
+  const center = useMemo(() => ({ lat: 44, lng: -80}), []);
+
+  return( 
+    <GoogleMap 
+      zoom={10}
+      center={center}
+      mapContainerClassName="map-container"
+    >
+      <Marker id="markPos" key="markKey" position={{lat: 44, lng: -80}} />
+      <div className="planner">
+        <div className="caret">^</div>
+      </div>
+      <div className="result-bar">hi</div>
+    </GoogleMap>
+    
+  );
+}
+
+// function Planner() {
+
+// }
+
 
 export default App;
