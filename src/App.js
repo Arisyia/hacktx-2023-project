c33dc65a6d3b28b537ef9fdc111d3ec3510e9f9a
unknown <faithnguyen814@gmail.com>
2023-10-21 16:57:44 -0500

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   const {isLoaded} = useLoadScript({googleMapsApiKey: API_KEY});
+  const [isExpanded, setIsExpanded] = useState(false);
 
+  function handleClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsExpanded(!isExpanded);
+  }
+
+
+  function Map() {
+    const center = useMemo(() => ({ lat: 44, lng: -80}), []);
+  
+    return( 
+      <GoogleMap 
+        zoom={10}
+        center={center}
+        mapContainerClassName="map-container"
+      >
+        <Marker id="markPos" key="markKey" position={{lat: 44, lng: -80}} />
+        <div className={`planner ${isExpanded ? 'expanded' : ''}`}>
+          <div className="caret" onClick={handleClick}>^</div>
+        </div>
+        <div className="result-bar">hi</div>
+      </GoogleMap>
+    );
+  }
+  
   if (!isLoaded) {
     return <div>Loading...</div>
   }
@@ -16,24 +43,6 @@ function App() {
   );
 }
 
-function Map() {
-  const center = useMemo(() => ({ lat: 44, lng: -80}), []);
-
-  return( 
-    <GoogleMap 
-      zoom={10}
-      center={center}
-      mapContainerClassName="map-container"
-    >
-      <Marker id="markPos" key="markKey" position={{lat: 44, lng: -80}} />
-      <div className="planner">
-        <div className="caret">^</div>
-      </div>
-      <div className="result-bar">hi</div>
-    </GoogleMap>
-    
-  );
-}
 
 // function Planner() {
 
