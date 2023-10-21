c0c7bb965738c2d58ef5ecf9e27b15236571a015
unknown <faithnguyen814@gmail.com>
2023-10-21 18:03:40 -0500

    progress?

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import './App.css';
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   const {isLoaded} = useLoadScript({googleMapsApiKey: API_KEY});
   const [isExpanded, setIsExpanded] = useState(false);
+  const [plannerChange, setPlannerChange] = useState(null);
+
+
+  const timer = ms => new Promise(res => setTimeout(res, ms));
+
+  // useEffect(() => {
+  //   createPlanner();
+  // }, []);
 
   function handleClick(e) {
-    e.preventDefault();
-    e.stopPropagation();
     setIsExpanded(!isExpanded);
+    // console.log(isExpanded);
+    // setPlannerChange(
+    //   <div className={`planner ${!isExpanded ? 'expanded' : ''}`}>
+    //     <div className="caret" onClick={handleClick}>^</div>
+    //   </div>);
+    // // if (isExpanded) {
+    // //   setIsExpanded(false);
+    // // } else {
+    // //   setIsExpanded(true);
+    // // }
+    // console.log(isExpanded);
+  }
+
+  function createPlanner() {
+    setPlannerChange(
+      <div className={`planner ${isExpanded ? 'expanded' : ''}`}>
+        <div className="caret" onClick={handleClick}>^</div>
+      </div>);
   }
 
 
@@ -25,10 +49,15 @@ function App() {
         mapContainerClassName="map-container"
       >
         <Marker id="markPos" key="markKey" position={{lat: 44, lng: -80}} />
-        <div className={`planner ${isExpanded ? 'expanded' : ''}`}>
-          <div className="caret" onClick={handleClick}>^</div>
+        {/* <div className="plannerChange">{plannerChange}</div> */}
+        setPlannerChange(
+            <div className={`planner ${!isExpanded ? 'expanded' : ''}`}>
+            <div className="caret" onClick={handleClick}>^</div>
+        </div>);
+        <div className="result-bar">
+          
+         {/* {places} */}
         </div>
-        <div className="result-bar">hi</div>
       </GoogleMap>
     );
   }
@@ -43,10 +72,6 @@ function App() {
   );
 }
 
-
-// function Planner() {
-
-// }
 
 
 export default App;
